99d82ba9c77990edfd9534c40eb5352bb7d0b030
Aria Taylor <ari@aricodes.net>
2024-11-10 12:10:35 -0800

    Create an immediate playlist file, reconnect on all errors

--- viewparty/main.py
+++ viewparty/main.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import random
 import subprocess
+import tempfile
 import datetime
 from time import sleep
 
@@ -31,47 +32,65 @@ async def _main(args):
 
     client = Client(TWITCH_TOKEN)
     user_id = (await client.fetch_users(names=[args.username]))[0].id
-    recently_watched = []
-
-    # Keep recently_watched under memory_length
-    if len(recently_watched) >= args.memory_length:
-        recently_watched.pop(0)
 
     videos = list(
         filter(
             lambda vid: "speedrun" in vid.title.lower()
-            and vid.url not in recently_watched
             and vid.published_at > datetime.datetime(2020, 1, 1).astimezone(),
             await client.fetch_videos(user_id=user_id, type="highlight"),
         )
     )
+    # Take a sampling of these videos and create a playlist file out of them
+    videos = random.choices(videos, k=10)
     with YoutubeDL(YDL_ARGS) as ydl:
-        video = random.choice(videos)
-        stream = subprocess.Popen(
-            [
-                "ffmpeg",
-                "-re",  # Playback in real time (useful for livestreaming, prevents skips)
-                "-i",
-                ydl.extract_info(video.url)["url"],  # Get the CDN URL for the playlist
-                "-codec",
-                "copy",  # Copy the audio and video unmodified
-                "-f",
-                "flv",  # Output to FLV (required for RTMP)
-                "-b:v",
-                "6M",  # Sets the video bitrate (max twitch supports)
-                "-b:a",
-                "48k",  # AssertionErrorets audio bitrate (max twitch supports)
-                "-reconnect_streamed",  # Reconnect on stream error
-                "1",
-                "-reconnect_on_network_error",  # Reconnect on network error
-                "1",
-                f"{args.rtmp_server}/{STREAM_KEY}",  # Actual RTMP target
-            ]
-        )
-        stream.wait()
+        # Map all the videos to their URLs
+        videos = [ydl.extract_info(video.url)["url"] for video in videos]
 
-        # Give the RTMP feed time to go down on Twitch's side
-        sleep(5)
+    playlist_file = tempfile.NamedTemporaryFile("w", delete=False)
+    playlist_file.write("\n".join([f"file '{url}'" for url in videos]))
+    playlist_file.close()
+
+    stream = subprocess.Popen(
+        [
+            "ffmpeg",
+            "-re",  # Playback in real time (useful for livestreaming, prevents skips)
+            "-f",
+            "concat",
+            "-safe",
+            "0",  # Accept HTTP streams
+            "-protocol_whitelist",
+            "file,http,https,tcp,tls",  # Expand protocol whitelist to accept web streams
+            "-i",
+            playlist_file.name,
+            "-codec",
+            "copy",  # Copy the audio and video unmodified
+            "-f",
+            "flv",  # Output to FLV (required for RTMP)
+            "-b:v",
+            "6M",  # Sets the video bitrate (max twitch supports)
+            "-b:a",
+            "48k",  # AssertionErrorets audio bitrate (max twitch supports)
+            "-reconnect_streamed",  # Reconnect on stream error
+            "1",
+            "-reconnect_on_network_error",  # Reconnect on network error
+            "1",
+            "-drop_pkts_on_overflow",
+            "1",
+            "-attempt_recovery",
+            "1",
+            "-recover_any_error",
+            "1",
+            f"{args.rtmp_server}/{STREAM_KEY}",  # Actual RTMP target
+        ]
+    )
+    return_code = stream.wait()
+    os.remove(playlist_file.name)
+
+    # Reboot immediately with no delay on failure
+    if return_code != 0:
+        return
+    # Otherwise give the RTMP feed time to go down on Twitch's side before rebooting
+    sleep(120)
 
 
 def main():
